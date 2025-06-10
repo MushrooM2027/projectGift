d4e49d386b3441774b4cd6746f984e6bf58d6f8c
Aditya Prakash <126641422+MushrooM2027@users.noreply.github.com>
2025-06-10 10:25:36 +0530

    Delete models/Users.js

--- models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-    // Add additional fields here if needed, e.g. name, createdAt, etc.
-});
-
-module.exports = mongoose.model('User', UserSchema);
